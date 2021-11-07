fc0033554a9895d5ac371367f04aab783abaf378
vgt <vgt@elca.vn>
2021-11-07 18:23:36 +0700

    First commit

---
+++ src/test/java/com/vuntt1412/epldashboard/EplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuntt1412.epldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EplDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
