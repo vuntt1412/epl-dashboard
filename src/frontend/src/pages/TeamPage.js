8970bbaebcc3ab75a526f979252838f60d0a1ef8
vgt <vgt@elca.vn>
2021-11-14 17:35:20 +0700

    add see-more-link and pie-chart for UI

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -3,6 +3,7 @@ import {React, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
 import {MatchDetailCard} from "../components/MatchDetailCard";
 import {MatchDetailSmallCard} from "../components/MatchDetailSmallCard";
+import {PieChart} from 'react-minimal-pie-chart';
 
 //specifically export this value
 export const TeamPage = () => {
@@ -36,15 +37,23 @@ export const TeamPage = () => {
                 <p>{team.latestMatches[0].leagueName}</p>
                 <div className="team-name"><h1>{team.teamLongName}</h1></div>
             </div>
-            <div className="team-statistic-section">Statistic</div>
+            <div className="team-statistic-section">
+                <PieChart
+                    data={[
+                        {title: 'Wins', value: 10, color: '#E38627'},
+                        {title: 'Losses', value: 15, color: '#C13C37'},
+                        {title: 'Ties', value: 20, color: '#6A2135'},
+                    ]}
+                />;
+            </div>
             <div className="match-detail-card-section">
                 <h5>Latest matches</h5>
                 <MatchDetailCard match={team.latestMatches[0]} teamName={teamName}/>
             </div>
             {team.latestMatches && team.latestMatches.slice(1).map(m => <MatchDetailSmallCard match={m}
                                                                                               teamName={teamName}/>)}
-            <div>
-                <a href="#">More</a>
+            <div className="more-link">
+                <a href="#">See More</a>
             </div>
         </div>
     );
